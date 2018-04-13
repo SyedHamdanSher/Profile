18947373ef4eb9cf497ba09f32f467cca86e2474
Syed Hamdan Sher <syedhamdansher@gmail.com>
2018-04-13 01:38:14 -0500

    add 4/13/18

--- DOM/script.js
+++ DOM/script.js
@@ -1,3 +1,6 @@
+var _ = require('lodash');
+
+console.log(_);
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
